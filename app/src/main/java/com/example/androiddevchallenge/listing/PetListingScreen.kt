64c3bafa59694b18ead6c99fac90ce41ba002ef2
Rebecca Franks <franks.rebecca@gmail.com>
2021-02-28 12:10:13 +0200

    Added cute lottie animation from https://lottiefiles.com/18265-my-french-bulldog

--- app/src/main/java/com/example/androiddevchallenge/listing/PetListingScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/listing/PetListingScreen.kt
@@ -15,44 +15,88 @@
  */
 package com.example.androiddevchallenge.listing
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.navigate
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieAnimationSpec
+import com.example.androiddevchallenge.Pet
 import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.Screen
+import com.example.androiddevchallenge.data.dog
+import com.example.androiddevchallenge.data.dog3
+import com.example.androiddevchallenge.data.dog4
 
 @Composable
 fun PetListingScreen(navController: NavController, viewModel: PetListingViewModel) {
     val pets = viewModel.petsData.observeAsState(emptyList())
+    PetListScreenContent(pets = pets.value) { pet ->
+        navController.navigate(Screen.PetDetailsScreen(pet.id).getCalculatedRoute())
+    }
+}
+@Preview
+@Composable
+fun PetListPreview(){
+    PetListScreenContent(pets = listOf(dog, dog3, dog4), onPetSelected = { /*TODO*/ })
+}
+
+@Composable
+fun PetListScreenContent(pets: List<Pet>, onPetSelected: (Pet) -> Unit){
     Surface(color = MaterialTheme.colors.background) {
         Column() {
-            Text(
-                text = stringResource(id = R.string.pet_adopt_title),
-                style = MaterialTheme.typography.h5,
-                modifier = Modifier.padding(
-                    start = 16.dp,
-                    end = 16.dp,
-                    top = 32.dp,
-                    bottom = 8.dp
+            Row(
+                modifier = Modifier.height(80.dp).fillMaxWidth().padding(end = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(id = R.string.pet_adopt_title),
+                    style = MaterialTheme.typography.h5,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(
+                        start = 16.dp,
+                        end = 16.dp,
+                        top = 32.dp,
+                        bottom = 8.dp
+                    )
                 )
-            )
+                val animationSpec = remember { LottieAnimationSpec.RawRes(R.raw.french_bulldog) }
+                LottieAnimation(
+                    animationSpec,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+
             PetGrid(
                 modifier = Modifier.padding(
                     start = 12.dp,
                     end = 12.dp
                 ),
-                pets.value
+                pets
             ) { pet ->
-                navController.navigate(Screen.PetDetailsScreen(pet.id).getCalculatedRoute())
+                onPetSelected(pet)
             }
         }
     }
