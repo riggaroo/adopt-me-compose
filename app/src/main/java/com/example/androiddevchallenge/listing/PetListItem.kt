5d4a9832831130091f1fe368af00f911cb7837fb
Rebecca Franks <franks.rebecca@gmail.com>
2021-02-28 11:08:59 +0200

    improve padding

--- app/src/main/java/com/example/androiddevchallenge/listing/PetListItem.kt
+++ app/src/main/java/com/example/androiddevchallenge/listing/PetListItem.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.Gender
@@ -93,7 +94,8 @@ fun PetCardListItem(pet: Pet, onPetClick: (Pet) -> Unit) {
             Text(
                 pet.name,
                 style = MaterialTheme.typography.h6,
-                modifier = Modifier.padding(start = 8.dp, end = 8.dp)
+                modifier = Modifier.padding(start = 8.dp, end = 8.dp),
+                fontWeight = FontWeight.SemiBold
             )
             Text(
                 pet.breed,
